a88d3e564a547965e481fc58bbbfc514a32d7d87
northross <rossnorthrup@yahoo.com>
2020-09-17 12:56:03 -0400

    logo added

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 const NavBar = () => {
   return (
     <div className='nav'>
-      <p className='logo'>Jiggr'd</p>
+      <img className='logo' src={logo} alt='logo' height='85' width='85' />
+      {/* <p className='logo'>Jigr</p> */}
       <ul>
         <li className='navLinks'>
           <Link to='/'>HOME</Link>
