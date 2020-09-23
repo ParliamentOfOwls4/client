f321214dfec6023a70d034ffdde198dd1eee9f06
northross <rossnorthrup@yahoo.com>
2020-09-23 14:37:37 -0400

    fixed navbar styling

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -5,16 +5,24 @@ import logo from '../assets/JiggyLogoTransparent.png';
 const NavBar = () => {
   return (
     <div className='nav'>
-      <a href="/"><img className='logo' src={logo} alt='logo' height='85' width='110' /></a>
+      <a href='/'>
+        <img className='logo' src={logo} alt='logo' height='85' width='110' />
+      </a>
       {/* <p className='logo'>Jigr</p> */}
-      <ul>
-        <li className='navLinks'>
-          <Link to='/'>HOME</Link>
-        </li>
-        <li className='navLinks'>
-          <Link to='/about'>ABOUT</Link>
-        </li>
-      </ul>
+      <Link
+        className='navLinks'
+        style={{ color: '#fff', textDecoration: 'none' }}
+        to='/'
+      >
+        HOME
+      </Link>
+      <Link
+        className='navLinks'
+        style={{ color: '#fff', textDecoration: 'none' }}
+        to='/about'
+      >
+        ABOUT
+      </Link>
     </div>
   );
 };
