b6f14f977818be3839594c20f2f6fd1156232dd5
x5se7en9x <tnansci@gmail.com>
2020-09-28 12:38:59 -0400

    accept changes for SearchByIngResult, DrinkList

--- src/Components/Utility/DrinkList.js
+++ src/Components/Utility/DrinkList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DrinkList = (props) => {
-<<<<<<< HEAD
   if (!props.drinkData) {
     return <div></div>;
   } else if (props.drinkData === 'None Found') {
@@ -17,22 +16,22 @@ const DrinkList = (props) => {
       <div className='drink-list-container'>
         {props.drinkData.map((drink) => (
           <ul className='drink-list-item-container' key={drink.idDrink}>
-              <img
-                  className='drink-list-image'
-                  src={`${drink.strDrinkThumb}`}
-                  height='80'
-                  width='80'
-                  alt='pic'
-                />
-            <li className='drink-list-text' key={drink.idDrink}>                
-                <Link
-                  to={{
-                    pathname: `/drink/${drink.idDrink}`,
-                    state: { id: `${drink.idDrink}` },
-                  }}
-                >
-                  {drink.strDrink}
-                </Link>
+            <img
+              className='drink-list-image'
+              src={`${drink.strDrinkThumb}`}
+              height='80'
+              width='80'
+              alt='pic'
+            />
+            <li className='drink-list-text' key={drink.idDrink}>
+              <Link
+                to={{
+                  pathname: `/drink/${drink.idDrink}`,
+                  state: { id: `${drink.idDrink}` },
+                }}
+              >
+                {drink.strDrink}
+              </Link>
             </li>
           </ul>
         ))}
@@ -40,38 +39,5 @@ const DrinkList = (props) => {
     );
   }
 };
-=======
-    if (!props.drinkData) {
-        return (<div></div>)
-    } else {
-        return (
-            <div>
-                {props.drinkData.map((drink) => (
-                    <ul>
-                        <li className='alcoholDrinkListLink' key={drink.idDrink}>
-                            <div className='alcoholDrinkList'>
-                                <img
-                                    src={`${drink.strDrinkThumb}`}
-                                    height='80'
-                                    width='80'
-                                    alt='pic'
-                                />
-                                <Link
-                                    to={{
-                                        pathname: `/drink/${drink.idDrink}`,
-                                        state: { id: `${drink.idDrink}` },
-                                    }}
-                                >
-                                    {drink.strDrink}
-                                </Link>
-                            </div>
-                        </li>
-                    </ul>
-                ))}
-            </div>
-        )
-    }
-}
->>>>>>> working on invalid search input
 
 export default DrinkList;
