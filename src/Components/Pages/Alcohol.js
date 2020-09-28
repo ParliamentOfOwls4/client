669f5fc6177badaecdb3b29be763c2784c049a95
x5se7en9x <tnansci@gmail.com>
2020-09-28 13:32:31 -0400

    working on formatting for alcohol description

--- src/Components/Pages/Alcohol.js
+++ src/Components/Pages/Alcohol.js
@@ -6,8 +6,11 @@ import DrinkList from '../Utility/DrinkList';
 
 const Alcohol = (props) => {
   const [alcohol, setAlcohol] = useState(null);
+  const [shortDescription, setShortDescription] = useState(null);
   const [alcoholDrinks, setAlcoholDrinks] = useState(null);
+  const [readMore, setReadMore] = useState(false)
   const selection = props.location.state.selection;
+  const readMoreCheck = readMore ? '..read less <<' : '..read more >>'
 
   // Make request to "Search cocktail by name" to get an "about this type of alcohol blurb"
   const configDescription = {
@@ -27,6 +30,8 @@ const Alcohol = (props) => {
   useEffect(() => {
     axios(configDescription)
       .then((res) => setAlcohol(res.data.ingredients[0]))
+      .then((res) => setShortDescription(res.data.ingredients[0].strDescription.substr(0, 200)))
+
       .catch(console.error);
     // eslint-disable-next-line
   }, []);
@@ -52,19 +57,34 @@ const Alcohol = (props) => {
     // eslint-disable-next-line
   }, []);
 
+
+
+
   if (!alcohol || !alcoholDrinks) {
     return <Loading />;
   } else {
+    // const shortDescription = alcohol.strDescription.substr(0, 200)
+    const { strDescription } = alcohol
+    // const fullDescription = alcohol.strDescription
+    console.log('here!!!', shortDescription);
     return (
       <Container>
         <div className='alcohol-page-title'>{selection}</div>
         <Row>
-          <Col xs={4} className='alcohol-page-description'>{alcohol.strDescription}</Col>
+
+          <Col xs={4} className='alcohol-page-description'> {readMore ? { strDescription } : { shortDescription }}
+
+            < a onClick={() => { setReadMore(!readMore) }}>{readMoreCheck} </a>
+
+          </Col>
+
+
           <Col xs={8} className='alcohol-page-drink-list'>
             <DrinkList drinkData={alcoholDrinks} />
           </Col>
+
         </Row>
-      </Container>
+      </Container >
     );
   }
 };
