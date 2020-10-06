f26afd12c8146b78c703a0a1efd7f00c07d28063
Danpowers24 <danpowers610@gmail.com>
2020-10-06 15:24:18 -0700

    Remove console warnings

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { BsPersonLinesFill } from 'react-icons/bs';
